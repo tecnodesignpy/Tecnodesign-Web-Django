f393b9188a92c387b882a8aff20b6774770d6dc6
ledezmatto <ledezmatto@tecnodesign.com.py>
2018-09-13 17:49:41 -0400

    Debug False

--- web/settings.py
+++ web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f+*8$vg1(b+ie7$+g6gq3ot4)y+_z(aegd*vehifq$e@8!bi#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
